35576eceac2c0ac704cd3cd277a9fe04d6a80b9f
Rick <rcsw.devel@gmail.com>
2026-02-11 15:43:03 -0800

    Initial Commit

---
+++ ShazamAppClone/ShazamAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamAppCloneApp.swift
+//  ShazamAppClone
+//
+//  Created by Rick Cheng on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct ShazamAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShazamAppCloneTests/ShazamAppCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamAppCloneTests.swift
+//  ShazamAppCloneTests
+//
+//  Created by Rick Cheng on 2/11/26.
+//
+
+import Testing
+@testable import ShazamAppClone
+
+struct ShazamAppCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
